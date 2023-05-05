b541b45caf9a350e67f5be3a6a7634676ae55876
comoser <david.alecrim1@gmail.com>
2023-05-05 16:22:25 +0100

    feat: add avatar

--- src/partials/Navbar.tsx
+++ src/partials/Navbar.tsx
@@ -1,10 +1,11 @@
 import {
-  Logo,
   NavbarTwoColumns,
   NavMenu,
   NavMenuItem,
   Section,
 } from 'astro-boilerplate-components';
+
+import { Logo } from '@/partials/Logo';
 
 const Navbar = () => (
   <Section>
@@ -12,31 +13,24 @@ const Navbar = () => (
       <a href="/">
         <Logo
           icon={
-            <svg
-              className="mr-1 h-10 w-10 stroke-cyan-600"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M0 0h24v24H0z" stroke="none"></path>
-              <rect x="3" y="12" width="6" height="8" rx="1"></rect>
-              <rect x="9" y="8" width="6" height="12" rx="1"></rect>
-              <rect x="15" y="4" width="6" height="16" rx="1"></rect>
-              <path d="M4 20h14"></path>
-            </svg>
+            <img
+              src="/assets/images/logo.png?nf_resize=fit&w=50&h=50"
+              alt="logo"
+              height="50"
+              width="50"
+            />
           }
           name="Get to know me"
         />
       </a>
 
-      <NavMenu>
-        <NavMenuItem href="/open-source">Open Source</NavMenuItem>
-        <NavMenuItem href="/articles">Articles</NavMenuItem>
-        <NavMenuItem href="/talks">Talks</NavMenuItem>
-      </NavMenu>
+      <div className="mt-2">
+        <NavMenu>
+          <NavMenuItem href="/open-source">Open Source</NavMenuItem>
+          <NavMenuItem href="/articles">Articles</NavMenuItem>
+          <NavMenuItem href="/talks">Talks</NavMenuItem>
+        </NavMenu>
+      </div>
     </NavbarTwoColumns>
   </Section>
 );
