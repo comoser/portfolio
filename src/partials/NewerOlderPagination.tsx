0b48cce5f88936a2c4c853022746032ac4d3ad73
comoser <david.alecrim1@gmail.com>
2023-05-17 10:10:06 +0100

    feat: add new project + fix pagination

---
+++ src/partials/NewerOlderPagination.tsx
@@ -0,0 +1,19 @@
+import type { FrontmatterPage } from '@/types/FrontMatter';
+
+type INewerOlderPaginationProps = {
+  type: 'Projects' | 'Articles' | 'Talks';
+  page: FrontmatterPage;
+};
+
+const NewerOlderPagination = (props: INewerOlderPaginationProps) => (
+  <div className="flex justify-center gap-8">
+    {props.page.url.prev && (
+      <a href={props.page.url.prev}>{`← Newer ${props.type}`}</a>
+    )}
+    {props.page.url.next && (
+      <a href={props.page.url.next}>{`Older ${props.type} →`}</a>
+    )}
+  </div>
+);
+
+export { NewerOlderPagination };
