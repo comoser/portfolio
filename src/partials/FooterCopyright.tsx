05088b0b19d6e38c2d882adf550221b3447f41df
comoser <david.alecrim1@gmail.com>
2023-06-20 08:15:43 +0100

    feat: remove link in footer

--- src/partials/FooterCopyright.tsx
+++ src/partials/FooterCopyright.tsx
@@ -1,3 +1,5 @@
+import { GradientText } from 'astro-boilerplate-components';
+
 type IFooterCopyrightProps = {
   site_name: string;
 };
@@ -6,16 +8,7 @@ const FooterCopyright = (props: IFooterCopyrightProps) => (
   <div className="border-t border-gray-600 pt-5">
     <div className="text-sm text-gray-200">
       © Copyright {new Date().getFullYear()} by {props.site_name}. Built with ♥
-      by{' '}
-      <a
-        className="text-cyan-400 hover:underline"
-        href="https://creativedesignsguru.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        David Alecrim
-      </a>
-      .
+      by <GradientText>David Alecrim</GradientText>.
     </div>
   </div>
 );
